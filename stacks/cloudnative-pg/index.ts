db22556c4463c855fd884221ac1f4d9a610df252
sapslaj <saps.laj@gmail.com>
2025-08-07 18:59:02 -0400

    setup cnpg

---
+++ stacks/cloudnative-pg/index.ts
@@ -0,0 +1,34 @@
+import * as kubernetes from "@pulumi/kubernetes";
+
+import { newK3sProvider } from "../../components/k3s-shared";
+
+const provider = newK3sProvider();
+
+const namespace = new kubernetes.core.v1.Namespace("cnpg-system", {
+  metadata: {
+    name: "cnpg-system",
+  },
+}, { provider });
+
+new kubernetes.helm.v3.Chart("cloudnative-pg", {
+  chart: "cloudnative-pg",
+  fetchOpts: {
+    repo: "https://cloudnative-pg.github.io/charts",
+  },
+  version: "0.25.0",
+  namespace: namespace.metadata.name,
+  skipCRDRendering: true,
+  values: {
+    crds: {
+      create: false,
+    },
+    monitoring: {
+      podMonitorEnabled: true,
+      grafanaDashboard: {
+        create: true,
+      },
+    },
+  },
+}, {
+  provider,
+});
