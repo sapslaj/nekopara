81b3868bd63b65f19c0935a2a18617b0077600ad
sapslaj <saps.laj@gmail.com>
2025-08-07 23:48:07 -0400

    setup Grafana with Authentik

--- stacks/grafana/index.ts
+++ stacks/grafana/index.ts
@@ -1,14 +1,77 @@
 import * as kubernetes from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
+import * as random from "@pulumi/random";
 
 import { newK3sProvider, transformSkipIngressAwait } from "../../components/k3s-shared";
 import { DNSRecord } from "../../components/shimiko/DNSRecord";
+import * as authentik from "../../sdks/authentik";
 
 const provider = newK3sProvider();
 
 const namespace = new kubernetes.core.v1.Namespace("grafana", {
   metadata: {
     name: "grafana",
+  },
+}, { provider });
+
+const clientID = new random.RandomId("client-id", {
+  byteLength: 16,
+});
+
+const authentikGroupAdmins = new authentik.Group("grafana-admins", {
+  name: "Grafana Admins",
+});
+
+const authentikGroupEditors = new authentik.Group("grafana-editors", {
+  name: "Grafana Editors",
+});
+
+const authentikGroupViewers = new authentik.Group("grafana-viewers", {
+  name: "Grafana Viewers",
+});
+
+const authentikProvider = new authentik.ProviderOauth2("grafana", {
+  name: "Grafana",
+  clientId: clientID.id,
+  authorizationFlow: authentik.getFlowOutput({
+    slug: "default-provider-authorization-implicit-consent",
+  }).id,
+  invalidationFlow: authentik.getFlowOutput({
+    slug: "default-provider-invalidation-flow",
+  }).id,
+  allowedRedirectUris: [
+    {
+      matching_mode: "strict",
+      url: "https://grafana.sapslaj.xyz/login/generic_oauth",
+    },
+  ],
+  propertyMappings: [
+    authentik.getPropertyMappingProviderScopeOutput({
+      name: "authentik default OAuth Mapping: OpenID 'email'",
+    }).id,
+    authentik.getPropertyMappingProviderScopeOutput({
+      name: "authentik default OAuth Mapping: OpenID 'profile'",
+    }).id,
+    authentik.getPropertyMappingProviderScopeOutput({
+      name: "authentik default OAuth Mapping: OpenID 'openid'",
+    }).id,
+  ],
+});
+
+const authentikApplication = new authentik.Application("grafana", {
+  name: "Grafana",
+  slug: "grafana",
+  protocolProvider: authentikProvider.providerOauth2Id.apply((id) => parseInt(id)),
+});
+
+const oauthSecret = new kubernetes.core.v1.Secret("grafana-auth-generic-oauth-secret", {
+  metadata: {
+    name: "grafana-auth-generic-oauth-secret",
+    namespace: namespace.metadata.name,
+  },
+  stringData: {
+    client_id: authentikProvider.clientId,
+    client_secret: authentikProvider.clientSecret,
   },
 }, { provider });
 
@@ -35,9 +98,41 @@ const grafana = new kubernetes.helm.v3.Chart("grafana", {
       enabled: true,
       storageClassName: "shortrack-aqua-exos",
     },
+    extraSecretMounts: [
+      {
+        name: "auth-generic-oauth-secret",
+        secretName: oauthSecret.metadata.name,
+        defaultMode: 400,
+        mountPath: "/etc/secrets/auth_generic_oauth",
+        readOnly: true,
+      },
+    ],
     plugins: [
       "victoriametrics-logs-datasource",
     ],
+    "grafana.ini": {
+      server: {
+        root_url: "https://grafana.sapslaj.xyz/",
+      },
+      auth: {
+        signout_redirect_url: pulumi
+          .interpolate`https://login.sapslaj.cloud/application/o/${authentikApplication.slug}/end-session/`,
+        oauth_auto_login: true,
+      },
+      "auth.generic_oauth": {
+        enabled: true,
+        name: "sapslaj cloud login",
+        allow_sign_up: true,
+        client_id: "$__file{/etc/secrets/auth_generic_oauth/client_id}",
+        client_secret: "$__file{/etc/secrets/auth_generic_oauth/client_secret}",
+        scopes: "openid profile email",
+        auth_url: "https://login.sapslaj.cloud/application/o/authorize/",
+        token_url: "https://login.sapslaj.cloud/application/o/token/",
+        api_url: "https://login.sapslaj.cloud/application/o/userinfo/",
+        role_attribute_path:
+          "contains(groups, 'Grafana Admins') && 'Admin' || contains(groups, 'Grafana Editors') && 'Editor' || 'Viewer'",
+      },
+    },
     sidecar: {
       dashboards: {
         enabled: true,
