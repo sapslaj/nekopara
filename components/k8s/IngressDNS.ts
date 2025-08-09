72945fe14c7cf4fd4a1fdcc67e6b4e4c2b5aeb61
sapslaj <saps.laj@gmail.com>
2025-08-09 13:45:23 -0400

    IngressDNS

---
+++ components/k8s/IngressDNS.ts
@@ -0,0 +1,80 @@
+import * as aws from "@pulumi/aws";
+import * as cloudflare from "@pulumi/cloudflare";
+import * as kubernetes from "@pulumi/kubernetes";
+import * as pulumi from "@pulumi/pulumi";
+import * as std from "@pulumi/std";
+
+import * as authentik from "@pulumi/authentik";
+import { getDnsFullName, getDnsHostName, newK3sProvider } from "../k3s-shared";
+import { DNSRecord } from "../shimiko/DNSRecord";
+
+export interface IngressDNSProps {
+  hostname?: pulumi.Input<string>;
+  ttl?: pulumi.Input<number>;
+}
+
+export class IngressDNS extends pulumi.ComponentResource {
+  hostname: pulumi.Output<string>;
+
+  constructor(name: string, props: IngressDNSProps = {}, opts: pulumi.ComponentResourceOptions = {}) {
+    super("sapslaj:nekopara:IngressDNS", name, {}, opts);
+
+    const hostname = pulumi.output(props.hostname ?? name).apply((hostname) => {
+      if (!hostname.includes(".")) {
+        hostname += ".sapslaj.xyz";
+      }
+
+      if (hostname.endsWith(".sapslaj.xyz")) {
+        new DNSRecord(name, {
+          name: hostname.replace(/\.sapslaj\.xyz$/, ""),
+          type: "CNAME",
+          records: ["homelab.sapslaj.com."],
+        }, {
+          parent: this,
+        });
+      } else if (hostname.endsWith(".sapslaj.cloud")) {
+        new aws.route53.Record(name, {
+          name: hostname,
+          ttl: props.ttl ?? 300,
+          type: "CNAME",
+          records: ["homelab.sapslaj.com."],
+          zoneId: aws.route53.getZoneOutput({
+            name: "sapslaj.cloud",
+          }).zoneId,
+        }, {
+          parent: this,
+        });
+      } else if (hostname.endsWith(".cloud.sapslaj.com")) {
+        new aws.route53.Record(name, {
+          name: hostname,
+          ttl: props.ttl ?? 300,
+          type: "CNAME",
+          records: ["homelab.sapslaj.com."],
+          zoneId: aws.route53.getZoneOutput({
+            name: "cloud.sapslaj.com",
+          }).zoneId,
+        }, {
+          parent: this,
+        });
+      } else if (hostname.endsWith(".sapslaj.com")) {
+        new cloudflare.DnsRecord(name, {
+          name: hostname.replace(/\.sapslaj\.com$/, ""),
+          type: "CNAME",
+          ttl: props.ttl ?? 300,
+          content: "homelab.sapslaj.com",
+          zoneId: cloudflare.getZoneOutput({
+            filter: {
+              name: "sapslaj.com",
+            },
+          }).id,
+        }, {
+          parent: this,
+        });
+      } else {
+        throw new Error(`unsupported zone for hostname ${hostname}`);
+      }
+    });
+
+    this.hostname = pulumi.output(hostname);
+  }
+}
