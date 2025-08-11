ecce90b5bab4bc4038f7a47de2acf9eeb09c7603
sapslaj <saps.laj@gmail.com>
2025-08-11 15:19:58 -0400

    set up Jaeger

---
+++ stacks/jaeger/index.ts
@@ -0,0 +1,168 @@
+import * as kubernetes from "@pulumi/kubernetes";
+import * as pulumi from "@pulumi/pulumi";
+import * as YAML from "yaml";
+
+import { newK3sProvider } from "../../components/k3s-shared";
+
+const provider = newK3sProvider();
+
+const namespace = new kubernetes.core.v1.Namespace("jaeger", {
+  metadata: {
+    name: "jaeger",
+  },
+}, { provider });
+
+const opensearch = new kubernetes.apiextensions.CustomResource("jaeger-opensearch", {
+  apiVersion: "opensearch.opster.io/v1",
+  kind: "OpenSearchCluster",
+  metadata: {
+    name: "jaeger-opensearch",
+    namespace: namespace.metadata.name,
+    annotations: {
+      "pulumi.com/patchForce": "true",
+    },
+  },
+  spec: {
+    general: {
+      image: "docker.io/opensearchproject/opensearch:2.3.0",
+      vendor: "Opensearch",
+      version: "2.3.0",
+      serviceName: "jaeger-opensearch",
+      drainDataNodes: true,
+      monitoring: {
+        enable: true,
+      },
+      disableSSL: true,
+    },
+    dashboards: {
+      enable: false,
+      replicas: 0,
+      version: "",
+    },
+    nodePools: [
+      {
+        component: "main",
+        replicas: 3,
+        diskSize: "100Gi",
+        roles: [
+          "master",
+          "data",
+        ],
+        resources: {
+          requests: {
+            memory: "2Gi",
+            cpu: "2",
+          },
+          limits: {
+            memory: "2Gi",
+            cpu: "2",
+          },
+        },
+        persistence: {
+          pvc: {
+            storageClass: "shortrack-aqua-exos",
+            accessModes: [
+              "ReadWriteOnce",
+            ],
+          },
+        },
+        env: [
+          {
+            name: "plugins.security.disabled",
+            value: "true",
+          },
+        ],
+      },
+    ],
+  },
+}, { provider });
+
+new kubernetes.core.v1.Pod("debug", {
+  metadata: {
+    name: "debug",
+    namespace: namespace.metadata.name,
+  },
+  spec: {
+    containers: [
+      {
+        name: "debug",
+        image: "ubuntu:latest",
+        command: [
+          "/bin/bash",
+          "-c",
+          "while :; do sleep 3600 ; done",
+        ],
+      },
+    ],
+  },
+}, { provider });
+
+const chart = new kubernetes.helm.v3.Chart("jaeger", {
+  chart: "jaeger",
+  fetchOpts: {
+    repo: "https://jaegertracing.github.io/helm-charts",
+  },
+  version: "3.4.1",
+  namespace: namespace.metadata.name,
+  skipCRDRendering: true,
+  values: {
+    provisionDataStore: {
+      cassandra: false,
+      elasticsearch: false,
+      kafka: false,
+    },
+    storage: {
+      type: "elasticsearch",
+      elasticsearch: {
+        host: "jaeger-opensearch",
+        anonymous: true,
+      },
+    },
+    collector: {
+      service: {
+        otlp: {
+          grpc: {
+            name: "otlp-grpc",
+            port: 4317,
+          },
+          http: {
+            name: "otlp-http",
+            port: 4318,
+          },
+        },
+      },
+    },
+    esIndexCleaner: {
+      enabled: true,
+    },
+    extraObjects: [
+      {
+        apiVersion: "v1",
+        kind: "ConfigMap",
+        metadata: {
+          name: "jaeger-grafana-ds",
+          labels: {
+            grafana_datasource: "1",
+          },
+        },
+        data: {
+          "datasource.yaml": YAML.stringify({
+            apiVersion: 1,
+            datasources: [
+              {
+                access: "proxy",
+                name: "Jaeger",
+                type: "jaeger",
+                // url: "http://victoria-logs-victoria-logs-single-server.victoria-metrics.svc.cluster.local.:9428",
+                // url: `{{ include "vm.url" (dict "helm" . "appKey" "vlselect" "style" "plain") }}`,
+                url: `http://{{ template "jaeger.query.name" . }}.{{ .Release.Namespace }}.svc.cluster.local:80`
+              },
+            ],
+          }),
+        },
+      },
+    ]
+  },
+}, {
+  provider,
+});
