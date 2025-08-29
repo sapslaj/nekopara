443f00cad04094ed3ca1843e3d4a10ac5c38cd25
sapslaj <saps.laj@gmail.com>
2025-08-29 17:13:44 -0400

    gitea OIDC

--- stacks/gitea/index.ts
+++ stacks/gitea/index.ts
@@ -2,15 +2,109 @@ import * as kubernetes from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
 import * as random from "@pulumi/random";
 
-import { newK3sProvider } from "../../components/k3s-shared";
+import { newK3sProvider, transformSkipIngressAwait } from "../../components/k3s-shared";
 import { IngressDNS } from "../../components/k8s/IngressDNS";
 import { Valkey } from "../../components/k8s/Valkey";
+import * as authentik from "../../sdks/authentik";
 
 const provider = newK3sProvider();
 
 const namespace = new kubernetes.core.v1.Namespace("gitea", {
   metadata: {
     name: "gitea",
+  },
+}, { provider });
+
+const clientID = new random.RandomId("client-id", {
+  byteLength: 16,
+});
+
+const authentikGroupAccess = new authentik.Group("gitea-access", {
+  name: "Gitea Access",
+});
+
+const authentikGroupUsers = new authentik.Group("gitea-users", {
+  name: "Gitea Users",
+});
+
+const authentikGroupAdmins = new authentik.Group("gitea-admins", {
+  name: "Gitea Admins",
+});
+
+const authentikGroupRestricted = new authentik.Group("gitea-restricted", {
+  name: "Gitea Restricted",
+});
+
+const authentikGroupScopeMapping = new authentik.ScopeMapping("gitea-group", {
+  name: "authentik gitea OAuth Mapping: OpenID 'gitea'",
+  scopeName: "gitea",
+  expression: `
+gitea_claims = {}
+
+if request.user.ak_groups.filter(name="Gitea Users").exists():
+    gitea_claims["gitea"]= "user"
+if request.user.ak_groups.filter(name="Gitea Admins").exists():
+    gitea_claims["gitea"]= "admin"
+if request.user.ak_groups.filter(name="Gitea Restricted").exists():
+    gitea_claims["gitea"]= "restricted"
+
+return gitea_claims
+`,
+});
+
+const authentikProvider = new authentik.ProviderOauth2("gitea", {
+  name: "Gitea",
+  clientId: clientID.id,
+  authorizationFlow: authentik.getFlowOutput({
+    slug: "default-provider-authorization-implicit-consent",
+  }).id,
+  invalidationFlow: authentik.getFlowOutput({
+    slug: "default-provider-invalidation-flow",
+  }).id,
+  allowedRedirectUris: [
+    {
+      matching_mode: "strict",
+      url: "https://git.sapslaj.cloud/user/oauth2/authentik/callback",
+    },
+  ],
+  propertyMappings: [
+    authentik.getPropertyMappingProviderScopeOutput({
+      name: "authentik default OAuth Mapping: OpenID 'email'",
+    }).id,
+    authentik.getPropertyMappingProviderScopeOutput({
+      name: "authentik default OAuth Mapping: OpenID 'profile'",
+    }).id,
+    authentik.getPropertyMappingProviderScopeOutput({
+      name: "authentik default OAuth Mapping: OpenID 'openid'",
+    }).id,
+    authentikGroupScopeMapping.id,
+  ],
+});
+
+const authentikApplication = new authentik.Application("gitea", {
+  name: "Gitea",
+  slug: "gitea",
+  protocolProvider: authentikProvider.providerOauth2Id.apply((id) => parseInt(id)),
+});
+
+new authentik.PolicyBinding("gitea-access", {
+  order: 100,
+  target: authentikApplication.uuid,
+  group: authentikGroupAccess.id,
+});
+
+const authentikSecret = new kubernetes.core.v1.Secret("gitea-authentik", {
+  metadata: {
+    name: "gitea-authentik",
+    namespace: namespace.metadata.name,
+    labels: {
+      "app.kubernetes.io/managed-by": "Pulumi",
+      "k3s.sapslaj.xyz/stack": "nekopara.gitea",
+    },
+  },
+  stringData: {
+    key: authentikProvider.clientId,
+    secret: authentikProvider.clientSecret,
   },
 }, { provider });
 
@@ -97,7 +191,7 @@ const chart = new kubernetes.helm.v3.Chart("gitea", {
   skipCRDRendering: true,
   values: {
     ingress: {
-      enabled: false,
+      enabled: true,
       className: "traefik",
       annotations: {
         "traefik.ingress.kubernetes.io/router.middlewares": "traefik-anubis@kubernetescrd",
@@ -170,7 +264,21 @@ const chart = new kubernetes.helm.v3.Chart("gitea", {
           enabled: true,
         },
       },
+      oauth: [
+        {
+          name: "authentik",
+          provider: "openidConnect",
+          existingSecret: authentikSecret.metadata.name,
+          autoDiscoverUrl: "https://login.sapslaj.cloud/application/o/gitea/.well-known/openid-configuration",
+          iconUrl: "https://login.sapslaj.cloud/static/dist/assets/icons/icon.png",
+          scopes: "email profile gitea",
+        },
+      ],
       config: {
+        server: {
+          PROTOCOL: "http",
+          ROOT_URL: "https://git.sapslaj.cloud",
+        },
         database: {
           DB_TYPE: "postgres",
         },
@@ -181,10 +289,19 @@ const chart = new kubernetes.helm.v3.Chart("gitea", {
         admin: {},
         security: {},
         camo: {},
-        openid: {},
-        oauth2_client: {},
+        openid: {
+          ENABLE_OPENID_SIGNIN: false,
+          ENABLE_OPENID_SIGNUP: false,
+        },
+        oauth2_client: {
+          ENABLE_AUTO_REGISTRATION: true,
+          UPDATE_AVATAR: true,
+        },
         service: {
           DISABLE_REGISTRATION: true,
+        },
+        "ssh.minimum_key_sizes": {
+          RSA: 2048,
         },
         webhook: {},
         mailer: {},
@@ -215,6 +332,9 @@ const chart = new kubernetes.helm.v3.Chart("gitea", {
   },
 }, {
   provider,
+  transforms: [
+    transformSkipIngressAwait(),
+  ],
 });
 
 new IngressDNS("git.sapslaj.cloud");
