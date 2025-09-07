f0936284ebb0297054ca02c2d519efdcbc42a471
sapslaj <saps.laj@gmail.com>
2025-09-07 11:20:51 -0400

    separate nfs provisioners for aqua and mitsuru

--- stacks/nfs-provisioner/index.ts
+++ stacks/nfs-provisioner/index.ts
@@ -1,11 +1,9 @@
 import * as kubernetes from "@pulumi/kubernetes";
 import * as pulumi from "@pulumi/pulumi";
 
-import { getDnsHostName, newK3sProvider } from "../../components/k3s-shared";
+import { newK3sProvider } from "../../components/k3s-shared";
 
 const provider = newK3sProvider();
-
-const dnsHostName = getDnsHostName();
 
 const namespace = new kubernetes.core.v1.Namespace("nfs-provisioner", {
   metadata: {
@@ -23,11 +21,55 @@ const nfsProvisioner = new kubernetes.helm.v3.Chart("nfs-subdir-external-provisi
   values: {
     nfs: {
       server: "172.24.4.10",
-      path: pulumi.interpolate`/mnt/exos/volumes/${dnsHostName}/nfs`,
+      path: "/mnt/exos/volumes/nekopara/nfs",
     },
     storageClass: {
       create: true,
       name: "nfs",
+      accessModes: "ReadWriteMany",
+    },
+  },
+}, {
+  provider,
+});
+
+const nfsProvisionerAqua = new kubernetes.helm.v3.Chart("nfs-subdir-external-provisioner-aqua", {
+  chart: "nfs-subdir-external-provisioner",
+  version: "4.0.18",
+  fetchOpts: {
+    repo: "https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner",
+  },
+  namespace: namespace.metadata.name,
+  values: {
+    nfs: {
+      server: "172.24.4.10",
+      path: "/mnt/exos/volumes/nekopara/nfs",
+    },
+    storageClass: {
+      create: true,
+      name: "nfs-aqua",
+      accessModes: "ReadWriteMany",
+    },
+  },
+}, {
+  provider,
+});
+
+const nfsProvisionerMitsuru = new kubernetes.helm.v3.Chart("nfs-subdir-external-provisioner-mitsuru", {
+  chart: "nfs-subdir-external-provisioner",
+  version: "4.0.18",
+  fetchOpts: {
+    repo: "https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner",
+  },
+  namespace: namespace.metadata.name,
+  values: {
+    nfs: {
+      server: "172.24.4.11",
+      path: "/red/nekopara/nfs",
+    },
+    storageClass: {
+      create: true,
+      name: "nfs-mitsuru",
       accessModes: "ReadWriteMany",
     },
   },
